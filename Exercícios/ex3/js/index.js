56ee4dd38dede1f5874538632eb99a2996f86c7f
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2023-10-26 17:31:56 -0300

    Update js

---
+++ Exercícios/ex3/js/index.js
@@ -0,0 +1,6 @@
+var n1 = Number.parseInt(window.prompt('Digit outro número'))
+var n2 = Number.parseInt(window.prompt('Digit outro número'))
+
+var s = n1 + n2
+
+window.prompt('O resultado foi: ' + s)
