9e4a60393e9d409ca05d12011548bde3b3bff44c
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2023-10-26 18:04:37 -0300

    Update js

--- Exercícios/ex3/js/index.js
+++ Exercícios/ex3/js/index.js
@@ -1,6 +1,6 @@
-var n1 = Number.parseInt(window.prompt('Digit outro número')) //Inteiro
-var n2 = Number.parseInt(window.prompt('Digit outro número')) //Inteiro
+var n1 = Number(window.prompt('Digit outro número')) //Int(Inteiro) ou Float(Real)
+var n2 = Number(window.prompt('Digit outro número')) //Int(Inteiro) ou Float(Real)
 
 var s = n1 + n2
 
-window.alert('O resultado foi: ' + s)
+window.alert(`O resultado entre ${n1} e ${n2} é igual a ${s}`) /*Uso de crase (``) para usar um ou mais placeholder (${})*/
