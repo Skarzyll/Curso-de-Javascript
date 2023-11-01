da138065e0ca6a0df6661dd87ee97b40a35184d6
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2023-11-01 18:06:32 -0300

    Update ex9

---
+++ Exercícios/ex09/index.js
@@ -0,0 +1,16 @@
+var pais = 'brasileiro'
+
+
+if (pais === 'BR' || pais === 'br' || pais === 'brasileiro' || pais === 'Brasileiro') {
+    console.log(`Você é brasileiro!`)
+} else {
+    console.log(`Você é estrangeiro!`)
+}
+
+var paIs = 'francês'
+
+if (paIs === 'FR' || paIs === 'fr' || paIs === 'Francês' || paIs === 'francês') {
+    console.log(`Você é Francês!`)
+} else {
+    console.log(`Você é estrangeiro!`)
+}
