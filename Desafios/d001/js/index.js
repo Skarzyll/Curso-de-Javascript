2aec5255181f5f800eb933b5a2b050c1fb982de8
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2023-11-06 16:32:11 -0300

    Update d001

---
+++ Desafios/d001/js/index.js
@@ -0,0 +1,11 @@
+function btn1() {
+    window.alert(`Você clicou no 1° botão!`)
+}
+
+function btn2() {
+    window.alert(`Você clicou no 2° botão!`)
+}
+
+function btn3() {
+    window.alert(`Você clicou no 3° botão!`)
+}
