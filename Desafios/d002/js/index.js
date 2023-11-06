1392ee82a031b67c6dc8a827ad2ca874791a2e01
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2023-11-06 19:31:51 -0300

    Update d002

---
+++ Desafios/d002/js/index.js
@@ -0,0 +1,5 @@
+function boasv() {
+    let nome = window.prompt(`Qual o seu nome?`)
+    let idade = window.prompt(`Olá, ${nome}! Quantos anos você têm?`)
+    window.alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`)
+}
