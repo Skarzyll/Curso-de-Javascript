33293e4038f984fdd9a3b8751e39c6ee5602964d
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2023-11-13 20:40:05 -0300

    Update ex19

--- Exercícios/ex19/js/index.js
+++ Exercícios/ex19/js/index.js
@@ -1,18 +1,36 @@
 function contar() {
-    var inicio = document.getElementById('inp1').value
-    var fim = document.getElementById('inp2').value
-    var passo = document.getElementById('inp3').value
+    var inicio = document.getElementById('inp1')
+    var fim = document.getElementById('inp2')
+    var passo = document.getElementById('inp3')
     var res = document.getElementById('divres')
 
-    if (inicio == '' || fim == '' || passo == '') {
+    if (inicio.value.length == '' || fim.value.length == '' || passo.value.length == '') {
         res.innerHTML = 'Impossível contar!'
-    } else if (passo == 0) {
-        window.alert('Passo invalido! Considerando o PASSO 1.')
-        passo = 1
 
-        while (inicio < fim) {
-            inicio = inicio + passo
-            res.innerHTML = 'Contando: ' + `${a} ` + `${inicio}`
+    } else if (passo.value == 0) {
+        window.alert('Passo invalido! Considerando o PASSO 1.')
+
+        passo.value = 1
+
+        res.innerHTML = 'Contando: '
+
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for (let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} `
+        }
+
+    } else {
+        res.innerHTML = 'Contando: '
+
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        for (let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} `
         }
     }
 }
