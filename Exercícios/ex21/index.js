381dff14cfa6a5718c4ffdbf303fa08ae9b53cb4
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2023-11-15 18:58:57 -0300

    Update ex21

--- Exercícios/ex21/index.js
+++ Exercícios/ex21/index.js
@@ -3,5 +3,5 @@ n.push(3)
 n.sort()
 
 console.log(n)
-console.log(`quantidade de volores em n é ${n.length}`)
-console.log(`valor de n na posição 0 é ${n[0]}`)
+console.log(`quantidade de volores no vetor n é ${n.length}`)
+console.log(`valor da posição 0 no vetor n é ${n[0]}`)
