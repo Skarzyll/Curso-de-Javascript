9732c718373adc7c4f877368d613d9175a70a556
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2023-11-21 18:35:22 -0300

    Update d005

--- Desafios/d005/js/index.js
+++ Desafios/d005/js/index.js
@@ -1,7 +1,30 @@
 let res = document.getElementById('res')
+let elem = document.createElement('h2')
 
 function Calc() {
     let valMetros = prompt('Digite uma distância em metros (m).')
 
-    
+    if (valMetros == 0 || valMetros.value == '') {
+        alert('Valor inválido. Por favor, tente novamente.')
+
+    } else {
+        res.innerHTML = ''
+        let valM = Number(valMetros)
+        let valQ = valM / 1000
+        let valH = valM / 100
+        let valDam = valM / 10
+        let valDm = valM * 10
+        let valC = valM * 100
+        let valMm = valM * 1000
+        
+        elem.innerHTML = `A distância de ${valM.toLocaleString('pt-BR')} metros, corresponde a...`
+        res.appendChild(elem)
+
+        res.innerHTML += `${valQ.toLocaleString('pt-BR')} quilômetros (km)<br>`
+        res.innerHTML += `${valH.toLocaleString('pt-BR')} hectomêtros (hm)<br>`
+        res.innerHTML += `${valDam.toLocaleString('pt-BR')} decâmetros (Dam)<br>`
+        res.innerHTML += `${valDm.toLocaleString('pt-BR')} decimetros (dm)<br>`
+        res.innerHTML += `${valC.toLocaleString('pt-BR')} centímetros (cm)<br>`
+        res.innerHTML += `${valMm.toLocaleString('pt-BR')} milímetros (mm)`
+    }
 }
