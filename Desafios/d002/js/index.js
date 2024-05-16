534ba63c2cdc709c02a5c0aeaed79cbb333c4560
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2024-05-15 22:11:43 -0300

    Changes for learning arrow functions cg 1

--- Desafios/d002/js/index.js
+++ Desafios/d002/js/index.js
@@ -1,5 +1,5 @@
-function boasv() {
-    let nome = window.prompt(`Qual o seu nome?`)
-    let idade = window.prompt(`Olá, ${nome}! Quantos anos você têm?`)
+let boasv = (nome, idade) => {
+    nome = window.prompt(`Qual o seu nome?`)
+    idade = window.prompt(`Olá, ${nome}! Quantos anos você têm?`)
     window.alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`)
 }
